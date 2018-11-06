776ca0903a60ecaa1e02934ae10dc48093bf42f9
DMANGENI <dorah_mangeni@putnam.com>
2018-11-06 13:06:59 -0500

    events-service spring boot application.

---
+++ src/main/java/com/eventz/eventzservice/EventzServiceApplication.java
@@ -0,0 +1,12 @@
+package com.eventz.eventzservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventzServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventzServiceApplication.class, args);
+	}
+}
